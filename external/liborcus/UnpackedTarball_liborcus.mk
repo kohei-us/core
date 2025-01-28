ba1247c0b33e9bb0aed28f04042010b637d98740
Xisco Fauli <xiscofauli@libreoffice.org>
2025-01-28 21:47:06 +0100

    mdds: upgrade to 3.0.0
    * external/mdds/0001-cid-1546160-COPY_INSTEAD_OF_MOVE.patch.1 is fixed upstream
    * external/mdds/use-after-free.patch is fixed upstream
    * external/liborcus/mdds3.0.patch.1 comes from
    https://gitlab.com/orcus/orcus/-/commit/bd861945d2a0273e5cff0edc0aadfd5fbbaac5f0
    
    Downloaded from https://gitlab.com/api/v4/projects/mdds%2Fmdds/packages/generic/source/3.0.0/mdds-3.0.0.tar.xz
    
    Change-Id: I44609a93ed002ed4aa9427457a8c5581d5f436ce

--- external/liborcus/UnpackedTarball_liborcus.mk
+++ external/liborcus/UnpackedTarball_liborcus.mk
@@ -18,6 +18,7 @@ $(eval $(call gb_UnpackedTarball_update_autoconf_configs,liborcus))
 $(eval $(call gb_UnpackedTarball_add_patches,liborcus,\
 	external/liborcus/rpath.patch.0 \
 	external/liborcus/libtool.patch.0 \
+	external/liborcus/mdds3.0.patch.1 \
 ))
 
 ifeq ($(OS),WNT)
